a8e64b1c29a36fadc9343a73baf5491b1b48ed96
twillis <qwillis1@outlook.com>
2023-03-27 08:16:51 +0200

    SlamNode and TfliteService - init

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM twillis1023/jetson_build_image
-WORKDIR /courage
-COPY . .
-CMD ["cmake"]
-EXPOSE 2200
--- docker-build.bat
+++
@@ -1,4 +0,0 @@
-docker build -t jetson_build_container_image .
-docker exec -it jetson_build_container_image bash
-:: docker run -dp 2200:2200 jetson_build_container_image
-:: ssh jetson@192.168.0.21
